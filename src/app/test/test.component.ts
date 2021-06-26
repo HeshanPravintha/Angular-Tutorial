a441d1b52bd4b5dd717cd7f48859617f8f98bb6c
HeshanPravintha <31812933+HeshanPravintha@users.noreply.github.com>
2021-06-26 12:04:28 +0530

    Interpolation

---
+++ src/app/test/test.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-test',
+  template: ' <h2> Hello {{name}} </h2> <h2>{{2+2}} </h2> <h2>{{"Welcome " + name}}</h2> <h2>{{name.length}}</h2> <h2>{{name.toUpperCase()}} </h2> <h2>{{greetUser()}}</h2> <h2>{{siteUrl}}</h2>',
+  styleUrls: []
+})
+export class TestComponent implements OnInit {
+
+  public name = "Heshan";
+  public siteUrl = window.location.href;
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  greetUser() {
+    return "Hello " + this.name; 
+  }
+}
